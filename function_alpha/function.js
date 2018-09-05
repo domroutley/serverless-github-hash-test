0e340c9ffe383a4c5e83513d7cece87a255b5569
domroutley <dominic.routley@gmail.com>
2018-09-05 15:17:30 +0100

    added aws auth to request options

--- function_alpha/function.js
+++ function_alpha/function.js
@@ -10,22 +10,36 @@ exports.handler = (event, context, callback) => {
 
 var callBeta = (event, callback) => {
   console.log('Calling beta')
+  var awsAuth = {
+    key: process.env.AWS_ACCESS_KEY_ID,
+    secret: process.env.AWS_SECRET_ACCESS_KEY,
+    session: process.env.AWS_SESSION_TOKEN,
+    sign_version: 4
+  }
   let options = {
     url: ENDPOINT.concat('beta'),
     method: 'POST',
     body: event,
-    json: true
+    json: true,
+    aws: awsAuth
   }
   send(options, callback)
 }
 
 var callGamma = (event, callback) => {
   console.log('Calling gamma')
+  var awsAuth = {
+    key: process.env.AWS_ACCESS_KEY_ID,
+    secret: process.env.AWS_SECRET_ACCESS_KEY,
+    session: process.env.AWS_SESSION_TOKEN,
+    sign_version: 4
+  }
   let options = {
     url: ENDPOINT.concat('gamma'),
     method: 'POST',
     body: event,
-    json: true
+    json: true,
+    aws: awsAuth
   }
   send(options, callback)
 }
