faa60cd1f609a57210045f1f20a8fa33ddd38476
domroutley <dominic.routley@gmail.com>
2018-09-18 12:34:29 +0100

    removed work files

--- awsmfa.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "WARNING: This will unset your AWS_PROFILE so that you can temporarily assume an AWS admin role"
-unset AWS_PROFILE
-awsprofile=$1
-mfa=$2
-
-export $(python .awsmfa.py $awsprofile $mfa)
